3aee3eb48917d08f3b8567a6e3108f80cedb6ea9
Alexey Rybalkin <alojscha.rybalkin@mail.ru>
2019-05-07 21:53:04 +0300

    add alarm button

--- apk/app/src/main/java/com/example/allergya/MainActivity.java
+++ apk/app/src/main/java/com/example/allergya/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.allergya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button changeToAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton()
+    {
+
+        changeToAlarm = findViewById(R.id.alarmButton);
+
+        changeToAlarm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intentAlarm = new Intent(".alarmActivity");
+                        startActivity(intentAlarm);
+                    }
+                }
+        );
     }
 }
